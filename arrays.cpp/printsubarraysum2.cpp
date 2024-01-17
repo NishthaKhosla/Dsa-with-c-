8a72fe15db16ab618a42ed6737e9d839600816b2
Nishtha Khosla <nishthakhosla77@gmail.com>
2024-01-17 22:12:16 +0530

    updated array

--- arrays.cpp/printsubarraysum2.cpp
+++ arrays.cpp/printsubarraysum2.cpp
@@ -35,7 +35,7 @@ int print_largestsum(int arr[], int n)
 
 int main()
 {
-    int arr[] = {1, 2, -3, 4, 5, 6, -7,8,9};
+    int arr[] = {1, 2, -3, 4, 5, 6, -7,8,9,10,11};
     int n = sizeof(arr)/sizeof(int);
 
     

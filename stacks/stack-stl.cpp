c0b4678bb9a1bf630a10bb3e858f5e2e42d16cb5
Nishtha Khosla <nishthakhosla77@gmail.com>
2024-02-27 23:39:25 +0530

    stack

--- stacks/stack-stl.cpp
+++ stacks/stack-stl.cpp
@@ -1,49 +1,55 @@
 /**atacks can be implemented using arrays,vectors,linkedlist
  * Arrays - are fixed size u need to check if the space is available
  * vector-if space ends,vectors doubles itself
- * linked-list-creates nodes and is dynamic 
-*/
-#include<iostream>
-#include<stack>
-#include<string>
+ * linked-list-creates nodes and is dynamic
+ */
+#include <iostream>
+#include <stack>
+#include <string>
 using namespace std;
 
-void insertAtBottom(stack<int> &s,int data){
-    if(s.empty()){
+void insertAtBottom(stack<int> &s, int data)
+{
+    if (s.empty())
+    {
         s.push(data);
         return;
     }
 
-    int temp=s.top();
+    int temp = s.top();
     s.pop();
 
-    insertAtBottom(s,data);
+    insertAtBottom(s, data);
     s.push(temp);
 };
 
-void reverse(stack<int>&s){
-if(s.empty()){
-    return;
+void reverse(stack<int> &s)
+{
+    if (s.empty())
+    {
+        return;
+    }
+
+    int t = s.top();
+    s.pop();
+    reverse(s);
+    insertAtBottom(s, t);
 }
 
-int t = s.top();
-s.pop();
-reverse(s);
-insertAtBottom(s,t);
-}
-
-int main(){
+int main()
+{
     stack<int> books;
     books.push(1);
     books.push(2);
     books.push(3);
-    books.push(4);
-   
-   reverse(books);
-   insertAtBottom(books,5);
+    books.push(7);
 
-    while(!books.empty()){
-        cout<<books.top()<<endl;
+    reverse(books);
+    insertAtBottom(books, 5);
+
+    while (!books.empty())
+    {
+        cout << books.top() << endl;
         books.pop();
     }
     return 0;
--- stacks/stackLL.h
+++ stacks/stackLL.h
@@ -1,42 +1,50 @@
 #include <string.h>
-template<typename T>
+template <typename T>
 class Stack;
 
-template<typename T>
-class Node{
-    public :
+template <typename T>
+class Node
+{
+public:
     T data;
-    Node<T>*next;
+    Node<T> *next;
 
-    Node(T d){
-        data=d;
+    Node(T d)
+    {
+        data = d;
     }
 };
 
-template<typename T>
-class Stack{
-    Node <T> *head;
+template <typename T>
+class Stack
+{
+    Node<T> *head;
 
-    public:
-    Stack(){
-        head=NULL;
+public:
+    Stack()
+    {
+        head = NULL;
     }
 
-    void push(T data){
-        Node <T> * n = new Node<T>(data);
-        n->next =head;
-        head =n;
+    void push(T data)
+    {
+        Node<T> *n = new Node<T>(data);
+        n->next = head;
+        head = n;
     }
 
-    Node<T>* top(){
+    Node<T> *top()
+    {
         return head;
     }
 
-    void pop(){
-        if(head!=NULL){
-        Node <T>* temp = head;
-        head = head->next;
-        delete temp;
+    void pop()
+    {
+        if (head != NULL)
+        {
+            Node<T> *temp = head;
+            head = head->next;
+            delete temp;
         }
     }
 };
